0f3af38d7f2faa0359904d1a239df2e41f5b76d1
lorenzopol <lorenzo.paolin.02@gmail.com>
2023-12-16 00:35:44 +0100

    fully working pdbToObj converter

--- PdbToObj.py
+++ PdbToObj.py
@@ -3,6 +3,8 @@ from Bio.PDB import PDBParser
 import BiologicalConstants as bc
 from dataclasses import dataclass
 import math
+import numpy as np
+import mcubes
 
 
 @dataclass
@@ -14,7 +16,17 @@ class PdbToObjConverter:
         self.parser = PDBParser()
         self.structure = self.parser.get_structure('protein', self.pdb_file_path)
         self.all_atom_coords = self.get_all_atom_coords()
+        self.all_atoms = self.get_all_atoms()
         self.back_bone_atom_coords = self.get_only_back_bone_atom_coords()
+
+    def get_all_atoms(self):
+        all_atom = []
+        for model in self.structure:
+            for chain in model:
+                for residue in chain:
+                    for atom in residue:
+                        all_atom.append(atom)
+        return all_atom
 
     def get_all_atom_coords(self):
         all_atom_coord = []
@@ -89,8 +101,7 @@ class PdbToObjConverter:
     @staticmethod
     def calculate_sphere_on_atom_coord(atom_coords: list | tuple, radius: int | float,
                                        n_slices: int, n_stack: int,
-                                       iteration: int = 0,
-                                       v_container: list | None = None, f_container: list | None = None):
+                                       iteration=0, v_container=None, f_container=None):
         """calculate vertex and faces based on atom_pos.
         > single use
         calculate_sphere_on_atom_coord(atom_coords=[x0, y0, z0], radius=0.2,
@@ -151,6 +162,41 @@ class PdbToObjConverter:
 
         return v_container, f_container
 
+    def calculate_sphere_on_bulk_atom_coords_list(self, atom_pos_container: tuple | list, radius,
+                                                  n_slices, n_stack):
+        """calculate vertex and faces based on atom_pos.
+                > single use
+                calculate_sphere_on_atom_coord(atom_coords=[[x0, y0, z0]], radius=0.2,
+                                               n_slices=7, n_stack=7)
+
+                > iterative use
+                calculate_sphere_on_atom_coord(atom_coords=[[x_i, y_i, z_i], ...], radius=0.2,
+                                               n_slices=7, n_stack=7,
+                                               iteration=i,
+                                               v_container=[], f_container=[])
+                returns v_container and f_container with the correctly populated data for vertex and faces value for obj dump"""
+
+        # there is only 1 atom to draw
+        if len(atom_pos_container) == 1:
+            v_container, f_container = self.calculate_sphere_on_atom_coord(
+                atom_pos_container, radius, n_slices, n_stack, 0)
+        else:
+            v_container = []
+            f_container = []
+            for index, atom_pos in enumerate(atom_pos_container):
+                v_container, f_container = self.calculate_sphere_on_atom_coord(
+                    atom_pos, radius, n_slices, n_stack, index, v_container, f_container)
+        return v_container, f_container
+
+    def calculate_sphere_on_bulk_atom_list(self):
+        """uses raw input from .pdb files and not manually parsed coords"""
+        v_container = []
+        f_container = []
+        for index, atom in enumerate(self.all_atoms):
+            v_container, f_container = self.calculate_sphere_on_atom_coord(
+                atom.coord, bc.relative_atom_radius.get(atom.element)/4, 7, 7, index, v_container, f_container)
+        return v_container, f_container
+
     @staticmethod
     def dump_v_f_container_to_obj_file(obj_file_handle, v_container, f_container):
         for point in v_container:
@@ -158,16 +204,34 @@ class PdbToObjConverter:
         for face in f_container:
             obj_file_handle.write(f"f {face[0]} {face[1]} {face[2]}\n")
 
-    def convert(self, obj_output_filepath):
-        v_container = []
-        f_container = []
-        for index, atom_pos in enumerate(self.all_atom_coords):
-            v_container, f_container = self.calculate_sphere_on_atom_coord(
-                atom_pos, 0.2, 9, 9, index, v_container, f_container)
-        with open(obj_output_filepath, "w") as obj_file_handle:
+    def dump_bond_containers_to_obj_file(self, obj_file_handle, inter_aa_bonds_rel, peptide_bonds_rel):
+        for point in self.all_atom_coords:
+            obj_file_handle.write(f"v {point[0]} {point[1]} {point[2]}\n")
+
+        for serial_number_atom_a, serial_number_atom_b in inter_aa_bonds_rel:
+            obj_file_handle.write(f"f {serial_number_atom_a} {serial_number_atom_b}\n")
+        for serial_number_atom_a, serial_number_atom_b in peptide_bonds_rel:
+            obj_file_handle.write(f"f {serial_number_atom_a} {serial_number_atom_b}\n")
+
+    def convert_atom_pos_from_coords(self, atom_coords_obj_output_filepath):
+        v_container, f_container = self.calculate_sphere_on_bulk_atom_coords_list(
+            self.all_atom_coords, 0.2, 11, 5)
+        with open(atom_coords_obj_output_filepath, "w") as obj_file_handle:
             self.dump_v_f_container_to_obj_file(obj_file_handle, v_container, f_container)
+
+    def convert_atom_pos(self, atom_coords_obj_output_filepath):
+        v_container, f_container = self.calculate_sphere_on_bulk_atom_list()
+        with open(atom_coords_obj_output_filepath, "w") as obj_file_handle:
+            self.dump_v_f_container_to_obj_file(obj_file_handle, v_container, f_container)
+
+    def convert_bond_pos(self, bond_coords_obj_output_filepath):
+        inter_aa_bonds_rel = self.compute_inter_aa_bonds_relationship()
+        peptide_bonds_rel = self.compute_peptide_bonds_relationship()
+        with open(bond_coords_obj_output_filepath, "w") as obj_file_handle:
+            self.dump_bond_containers_to_obj_file(obj_file_handle, inter_aa_bonds_rel, peptide_bonds_rel)
 
 
 if __name__ == "__main__":
     conv = PdbToObjConverter(r"C:\Users\loren\PycharmProjects\SimBCR-v2\pdb_files\first_try.pdb")
-    conv.convert("obj_files/first_try.obj")
+    conv.convert_atom_pos("obj_files/atom_coords.obj")
+    conv.convert_bond_pos("obj_files/bond_coords.obj")
